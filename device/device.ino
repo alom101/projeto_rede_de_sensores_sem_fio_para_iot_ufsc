0af67daa587144deda91fe588d5b3e20626736e3
Arthur Osorio <osorioarthur@gmail.com>
2025-05-31 23:14:14 -0300

    OTA support added

--- device/device.ino
+++ device/device.ino
@@ -1,14 +1,18 @@
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
 
+// Sensor
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
+
 #define DHT_PIN D5
 #define DHT_TYPE DHT11
 DHT_Unified dht(DHT_PIN, DHT_TYPE);
+//#define SAMPLE_RATE 60*1000 //ms
 
+// Device
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 #include "credentials.hpp"
 const char* ssid = WIFI_SSID;
@@ -16,7 +20,7 @@ const char* password = WIFI_PASSWORD;
 
 const char* mqtt_server = BROKER_IP;
 
-#define SEND_TIME 10*60*1000 //ms
+#define SEND_TIME 10*1000 //ms
 
 
 WiFiClient espClient;
@@ -34,6 +38,20 @@ void setup_wifi() {
   randomSeed(micros());
 }
 
+// OTA
+#include <ArduinoOTA.h>
+
+
+void OTA_init(){
+  ArduinoOTA.setPort(OTA_PORT);
+  ArduinoOTA.setHostname(OTA_NAME);
+  ArduinoOTA.setPassword(OTA_PASSWORD);
+//  ArduinoOTA.setPasswordHash(OTA_PASSWORD_HASH);
+  ArduinoOTA.begin();
+}
+
+
+
 void blink(){
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
@@ -46,6 +64,13 @@ void  callback(char* topic, byte* payload, unsigned int length) {
   if(String(topic)=="led"){
     if((char)payload[0] == '1') digitalWrite(LED_BUILTIN, LOW);
     if((char)payload[0] == '0') digitalWrite(LED_BUILTIN, HIGH);
+    if((char)payload[0] == 'b'){
+      int state = digitalRead(LED_BUILTIN);
+      blink();
+      blink();
+      blink();
+      digitalWrite(LED_BUILTIN, state);
+    }
   }
 }
 
@@ -77,6 +102,8 @@ void setup() {
   client->setServer(mqtt_server, 1883);
   client->setCallback(callback);
   client->subscribe("led");
+
+  OTA_init();
 }
 
 void send_data(){
@@ -97,7 +124,6 @@ void send_data(){
     }
   }
 }
-
 void update_client(){
   if (!client->connected()) {
     reconnect_mqtt();
@@ -106,6 +132,7 @@ void update_client(){
 }
 
 void loop() {
+  ArduinoOTA.handle();
   update_client();
   send_data();
 }
